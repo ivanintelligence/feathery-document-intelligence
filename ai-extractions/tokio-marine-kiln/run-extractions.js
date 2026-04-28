0cdcca7aaaef7bbb02bcacc99f3f4771c6999ecb
John Ivan <diazjohnivantuyor@gmail.com>
2026-04-28 16:31:22 +0800

    Commit

--- ai-extractions/tokio-marine-kiln/run-extractions.js
+++ ai-extractions/tokio-marine-kiln/run-extractions.js
@@ -1,41 +1,24 @@
 const id1 = '04aee576-70f2-44dd-b41b-c8965a61a3de'; // non-spreadsheet
 const id2 = 'e49c5e31-cb4b-4971-bf51-28d38de3bae6'; // spreadsheet
 
-const rawValue = FileUpload.value;
+const value = String(fileType.value || '').toLowerCase();
 
-const files = Array.isArray(rawValue)
-  ? await Promise.all(rawValue)
-  : [await rawValue];
+const hasSpreadsheet =
+  value.includes('xls') ||
+  value.includes('xlsx') ||
+  value.includes('xlsm') ||
+  value.includes('csv') ||
+  value.includes('spreadsheet') ||
+  value.includes('excel');
 
-const noSpreadsheet = files.some((file) => {
-  const name = file?.name?.toLowerCase() || '';
-  const type = file?.type?.toLowerCase() || '';
+const extractionIds = hasSpreadsheet ? [id2] : [id1, id2];
 
-  const isSpreadsheet =
-    name.endsWith('.xls') ||
-    name.endsWith('.xlsx') ||
-    type.includes('spreadsheetml') ||
-    type.includes('excel');
-
-  return !isSpreadsheet;
-});
-
-if (noSpreadsheet) {
-  const extractionIds = [id1, id2];
-
-  await Promise.all(
-    extractionIds.map((extractionId) =>
-      feathery.runAIExtraction(extractionId, {
-        waitForCompletion: true,
-        variantId: undefined,
-        pages: undefined
-      })
-    )
-  );
-} else {
-  await feathery.runAIExtraction(id2, {
-    waitForCompletion: true,
-    variantId: undefined,
-    pages: undefined
-  });
-}
+await Promise.all(
+  extractionIds.map((extractionId) =>
+    feathery.runAIExtraction(extractionId, {
+      waitForCompletion: true,
+      variantId: undefined,
+      pages: undefined
+    })
+  )
+);
